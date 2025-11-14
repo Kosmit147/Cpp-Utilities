ac2732d0f2ffce66f14f1f6977b1384c37d2fd0d
Wojciech Opara <wwojtek660@gmail.com>
2025-11-14 13:53:50 +0100

    Heterogeneous Lookup

---
+++ heterogeneous_lookup.hpp
@@ -0,0 +1,25 @@
+#include <string_view>
+#include <string>
+#include <cstddef>
+#include <utility>
+#include <functional>
+#include <unordered_map>
+#include <memory>
+
+struct StringHash
+{
+    using is_transparent = void;
+
+    [[nodiscard]] auto operator()(const char* text) const -> std::size_t { return std::hash<std::string_view>{}(text); }
+    [[nodiscard]] auto operator()(std::string_view text) const -> std::size_t { return std::hash<std::string_view>{}(text); }
+    [[nodiscard]] auto operator()(const std::string& text) const -> std::size_t { return std::hash<std::string>{}(text); }
+};
+
+// Use std::hash by default.
+template<typename T> struct Hash : std::hash<T> {};
+
+// String specialization to enable transparent lookup in unordered containers.
+template<> struct Hash<String> : StringHash {};
+
+template<typename Key, typename T, typename Hasher = Hash<Key>, typename KeyEqual = std::equal_to<>, typename Allocator = std::allocator<std::pair<const Key, T>>>
+using UnorderedMap = std::unordered_map<Key, T, Hasher, KeyEqual, Allocator>;
